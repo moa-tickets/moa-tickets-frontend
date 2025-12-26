ac81824fdc321ed866bbcddf1791b28014048b14
rukadeveloper <kansaki135@gmail.com>
2025-12-25 23:42:17 -0800

    feat:init 스펠링 고치기

--- src/FrontApplication.tsx
+++ src/FrontApplication.tsx
@@ -1,5 +1,5 @@
-import QueryProvider from "./app/provider/queryProvider";
-import RouteProvider from "./app/provider/RouteProvider";
+import QueryProvider from './app/provider/queryProvider';
+import RouteProvider from './app/provider/RouteProvider';
 
 function FrontApplication() {
   return (
