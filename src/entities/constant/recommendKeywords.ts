0bca6bfc927f180960588cbb4bbaf557c29d2f75
rukadeveloper <kansaki135@gmail.com>
2025-12-29 16:16:10 -0800

    chore: recommendKeys에 id 추가

--- src/entities/constant/recommendKeywords.ts
+++ src/entities/constant/recommendKeywords.ts
@@ -5,6 +5,7 @@ export const recommendKeywords: RecommendKeywordsType = {
     icons: '/icon/thumb.svg',
     contents: [
       {
+        id: 1,
         imgUrl:
           'https://ticketimage.interpark.com/Play/image/large/25/25016884_p.gif',
         title: '[2025 전시지원] 쿠푸왕의 피라미드',
@@ -12,6 +13,7 @@ export const recommendKeywords: RecommendKeywordsType = {
         date: ['2025.9.14', '2025.12.31'],
       },
       {
+        id: 2,
         imgUrl:
           'https://ticketimage.interpark.com/Play/image/large/25/25016884_p.gif',
         title: '[2025 전시지원] 쿠푸왕의 피라미드',
@@ -19,6 +21,7 @@ export const recommendKeywords: RecommendKeywordsType = {
         date: ['2025.9.14', '2025.12.31'],
       },
       {
+        id: 3,
         imgUrl:
           'https://ticketimage.interpark.com/Play/image/large/25/25016884_p.gif',
         title: '[2025 전시지원] 쿠푸왕의 피라미드',
@@ -26,6 +29,7 @@ export const recommendKeywords: RecommendKeywordsType = {
         date: ['2025.9.14', '2025.12.31'],
       },
       {
+        id: 4,
         imgUrl:
           'https://ticketimage.interpark.com/Play/image/large/25/25016884_p.gif',
         title: '[2025 전시지원] 쿠푸왕의 피라미드',
@@ -33,6 +37,7 @@ export const recommendKeywords: RecommendKeywordsType = {
         date: ['2025.9.14', '2025.12.31'],
       },
       {
+        id: 5,
         imgUrl:
           'https://ticketimage.interpark.com/Play/image/large/25/25016884_p.gif',
         title: '[2025 전시지원] 쿠푸왕의 피라미드',
@@ -40,6 +45,7 @@ export const recommendKeywords: RecommendKeywordsType = {
         date: ['2025.9.14', '2025.12.31'],
       },
       {
+        id: 6,
         imgUrl:
           'https://ticketimage.interpark.com/Play/image/large/25/25016884_p.gif',
         title: '[2025 전시지원] 쿠푸왕의 피라미드',
