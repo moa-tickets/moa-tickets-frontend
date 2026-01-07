e90030a861a5955cadab8b2982f3bda0d6bd74ec
rukadeveloper <kansaki135@gmail.com>
2026-01-06 17:18:20 -0800

    임시 URL 수정

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -3,8 +3,19 @@ import { cn } from '@/shared';
 import MainBannerSlides from '@/widgets/main-banner-slides/MainBannerSlides';
 import BannerGrid from '@/widgets/banner-grid/BannerGrid';
 import MainContentsWrapper from '@/shared/components/main-contents-wrapper/MainContentsWrapper';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 const MainPage = () => {
+  useEffect(() => {
+    axios
+      .get('https://app.moatickets.dev/health', {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  }, []);
   return (
     <div className={cn('main__page w-full')}>
       <MainBannerSlides />
