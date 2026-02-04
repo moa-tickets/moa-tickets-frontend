bca417b6e3909baf8908a536da65fae7966c353b
rukadeveloper <kansaki135@gmail.com>
2026-02-04 01:29:38 -0800

    fix: 테스트 케이스 초기화

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          jsx: 'react-jsx',
+          esModuleInterop: true,
+          allowSyntheticDefaultImports: true,
+        },
+      },
+    ],
   },
   collectCoverage: true,
 };
