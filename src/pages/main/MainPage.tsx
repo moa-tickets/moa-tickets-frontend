bb250cd29526383dd487d285a0f5128c9a5ae6e5
rukadeveloper <kansaki135@gmail.com>
2026-02-08 17:42:41 -0800

    fix: 메인 슬라이드 삭제

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,19 +1,11 @@
 import { cn } from '@/shared';
 
 import MainBannerSlides from '@/widgets/main-banner-slides/MainBannerSlides';
-import MainContentsWrapper from '@/shared/components/main-contents-wrapper/MainContentsWrapper';
 
 const MainPage = () => {
   return (
     <div className={cn('main__page w-full')}>
       <MainBannerSlides />
-      <MainContentsWrapper title="콘서트 랭킹" isAll={'콘서트 랭킹 전체보기'} />
-      <MainContentsWrapper
-        title="오픈 예정"
-        isAll={'오픈 예정 공연 전체보기'}
-      />
-      <MainContentsWrapper title="플레이" isTab />
-      <MainContentsWrapper title="추천 키워드" isTab />
     </div>
   );
 };
