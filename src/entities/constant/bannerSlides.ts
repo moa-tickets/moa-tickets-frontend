1c9e9f0617946a802213739fbaa406ca101fbbf9
rukadeveloper <kansaki135@gmail.com>
2026-02-09 23:30:10 -0800

    fix: mock 파일 webp에서 png로 바꾸기

--- src/entities/constant/bannerSlides.ts
+++ src/entities/constant/bannerSlides.ts
@@ -1,31 +1,31 @@
 export const bannerSlides = [
   {
     id: 1,
-    imageUrl: '/small_banner/banner_slides_1.webp',
+    imageUrl: '/small_banner/banner_slides_1.png',
     linkUrl: '/',
     alt: '연말세일 선착순 50% 쿠폰',
   },
   {
     id: 2,
-    imageUrl: '/small_banner/banner_slides_2.webp',
+    imageUrl: '/small_banner/banner_slides_2.png',
     linkUrl: '/',
     alt: '놀라운 2025-2026 신년 공연 특가 & 쿠폰',
   },
   {
     id: 3,
-    imageUrl: '/small_banner/banner_slides_3.webp',
+    imageUrl: '/small_banner/banner_slides_3.png',
     linkUrl: '/',
     alt: '우수 회원이라면 Gold Class 승급',
   },
   {
     id: 4,
-    imageUrl: '/small_banner/banner_slides_4.webp',
+    imageUrl: '/small_banner/banner_slides_4.png',
     linkUrl: '/',
     alt: '궤도 & 미스터 캬라멜과 일본에서 놀 사람 모집!',
   },
   {
     id: 5,
-    imageUrl: '/small_banner/banner_slides_5.webp',
+    imageUrl: '/small_banner/banner_slides_5.png',
     linkUrl: '/',
     alt: 'MOA 카드 4만원 캐시백 이벤트',
   },
