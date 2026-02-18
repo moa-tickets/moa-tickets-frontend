52863e2a7719768a281a3c7e32fbb779567042df
rukadeveloper <kansaki135@gmail.com>
2026-02-18 02:15:35 -0800

    feature: Alarm Container 추가 + 알람 등장 시점 고민 중

--- src/shared/components/layouts/MainLayout.tsx
+++ src/shared/components/layouts/MainLayout.tsx
@@ -2,10 +2,12 @@ import { cn } from '@/shared';
 import TopHeader from '../header/TopHeader';
 import BottomHeader from '../header/BottomHeader';
 import { Outlet } from 'react-router-dom';
+import AlarmContainer from '@/widgets/alarm-container/AlarmContainer';
 
 const MainLayout = () => {
   return (
     <div className={cn('main__layout')}>
+      <AlarmContainer />
       <TopHeader />
       <BottomHeader />
       <Outlet />
---
+++ src/widgets/alarm-container/AlarmContainer.tsx
@@ -0,0 +1,3 @@
+export default function AlarmContainer() {
+  return <div></div>;
+}
