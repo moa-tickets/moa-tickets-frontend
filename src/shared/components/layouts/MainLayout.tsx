5d9029881506fd76cca95247b22df1db5f602c44
rukadeveloper <kansaki135@gmail.com>
2026-02-18 15:54:12 -0800

    fix: SSE Connect Debug

--- src/shared/components/layouts/MainLayout.tsx
+++ src/shared/components/layouts/MainLayout.tsx
@@ -1,13 +1,19 @@
 import { cn } from '@/shared';
+import { useSelector } from 'react-redux';
 import TopHeader from '../header/TopHeader';
 import BottomHeader from '../header/BottomHeader';
 import { Outlet } from 'react-router-dom';
 import AlarmContainer from '@/widgets/alarm-container/AlarmContainer';
+import type { LoginState } from '@/entities/reducers/LoginReducer';
 
 const MainLayout = () => {
+  const { isLoggedIn } = useSelector(
+    (state: { loginReducer: LoginState }) => state.loginReducer,
+  );
+
   return (
     <div className={cn('main__layout')}>
-      <AlarmContainer />
+      {isLoggedIn && <AlarmContainer />}
       <TopHeader />
       <BottomHeader />
       <Outlet />
--- src/widgets/alarm-container/AlarmContainer.tsx
+++ src/widgets/alarm-container/AlarmContainer.tsx
@@ -1,3 +1,40 @@
+import { useEffect, useRef, useState } from 'react';
+
 export default function AlarmContainer() {
+  const eventSourceRef = useRef<EventSource | null>(null);
+  const [isConnected, setIsConnected] = useState<boolean>(false);
+  const [sseMessageList, setSseMessageList] = useState([]);
+
+  useEffect(() => {
+    const handleSseRequest = () => {
+      const eventSource = new EventSource('/api/alarm/sub', {
+        withCredentials: true,
+      });
+
+      eventSource.onopen = () => {
+        console.log('SSE connection opened');
+        setIsConnected(true);
+        eventSourceRef.current = eventSource;
+      };
+
+      eventSource.onerror = (event) => {
+        console.error('SSE connection error:', event);
+      };
+
+      eventSource.addEventListener('CONNECT', () => {
+        console.log('SSE CONNECT event received:');
+      });
+    };
+
+    handleSseRequest();
+
+    return () => {
+      if (eventSourceRef.current) {
+        eventSourceRef.current.close();
+        eventSourceRef.current = null;
+      }
+    };
+  }, []);
+
   return <div></div>;
 }
