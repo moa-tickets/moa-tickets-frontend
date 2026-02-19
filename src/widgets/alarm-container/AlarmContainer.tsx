186a4b55bd9f0a67b50cfffcbf810fc9b6bfd010
rukadeveloper <kansaki135@gmail.com>
2026-02-18 17:13:42 -0800

    fix: Alarm useState 임시 제거

--- src/widgets/alarm-container/AlarmContainer.tsx
+++ src/widgets/alarm-container/AlarmContainer.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 
 export default function AlarmContainer() {
   const eventSourceRef = useRef<EventSource | null>(null);
